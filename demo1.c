c7207228cc28e36e145b11e2d8e1096032bd5833
Dark Wang <dkfire30@outlook.com>
2020-01-11 12:04:10 +0800

    File copy
    Resolve #1
    
    Signed-off-by: Dark Wang <dkfire30@outlook.com>

--- demo1.c
+++ demo1.c
@@ -1,4 +1,31 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <zconf.h>
+
 int main()
 {
-	return 1;
+	int i = 0;
+	int o = 0;
+	const char *in = "../in.txt";
+	const char *out = "../out.txt";
+
+	i = open(in, O_RDONLY);
+	if (i < 0) {
+		perror(in);
+		exit(1);
+	}
+	o = open(out, O_WRONLY | O_CREAT, S_IRWXU);
+	if (o < 0) {
+		perror(out);
+		exit(1);
+	}
+
+	char buffer[128];
+	while (read(i, buffer, 1) > 0)
+		write(o, buffer, 1);
+
+	close(i);
+	close(o);
+	return 0;
 }
